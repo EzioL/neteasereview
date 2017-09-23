8c49c1ed050ac89c002bf10accf71abe3eeb7ecd
Ezio <814711679@qq.com>
2017-09-23 09:13:37 +0800

    Initial commit

---
+++ src/main/java/com/ezio/NeteasereviewApplication.java
@@ -0,0 +1,12 @@
+package com.ezio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeteasereviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeteasereviewApplication.class, args);
+	}
+}
